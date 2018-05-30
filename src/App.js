900cef7f9bd0f9987ccbb0264cdbcf89c23318e5
Shushanskii Konstantin <konstantin.shushanskii@alpari.org>
2018-05-30 23:06:07 +0300

    initial page-proof

--- src/App.js
+++ src/App.js
@@ -5,7 +5,34 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-
+          <table className={'table'}>
+            <tbody>
+              <tr>
+                  <td><div className={'chip'}>1</div></td>
+                  <td><div className={'chip'}>2</div></td>
+                  <td><div className={'chip'}>3</div></td>
+                  <td><div className={'chip'}>4</div></td>
+              </tr>
+              <tr>
+                  <td><div className={'chip'}>5</div></td>
+                  <td><div className={'chip'}>6</div></td>
+                  <td><div className={'chip'}>7</div></td>
+                  <td><div className={'chip'}>8</div></td>
+              </tr>
+              <tr>
+                  <td><div className={'chip'}>9</div></td>
+                  <td><div className={'chip'}>10</div></td>
+                  <td><div className={'chip'}>11</div></td>
+                  <td><div className={'chip'}>12</div></td>
+              </tr>
+              <tr>
+                  <td><div className={'chip'}>13</div></td>
+                  <td><div className={'chip'}>14</div></td>
+                  <td><div className={'chip'}>15</div></td>
+                  <td/>
+              </tr>
+            </tbody>
+          </table>
       </div>
     );
   }
